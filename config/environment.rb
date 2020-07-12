1ec0a747582eef0d7ca3926e6abc88f2df711d30
neivyx <janeivyhilario@gmail.com>
2020-07-12 01:45:23 -0400

    +# configures our program

---
+++ config/environment.rb
@@ -0,0 +1,20 @@
+require "netflix_mustwatch/version"
+require "netflix_mustwatch/cli"
+
+
+
+
+
+
+#this file is being required in /bin/console.rb
+#this file requires everything in the lib/netflix-mustwatch directory, hence the version require above.
+#in the lib/netflix_mustwatch -- this is where we will put things like our CLI class, class, scrapper etc.
+
+
+# module NetflixMustwatch "we dont need this module"
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+
+#renamed from netflix_mustwatch.rb 
---
+++ lib/cli.rb
@@ -0,0 +1,7 @@
+class Netflix_Mustwatch::CLI 
+    
+    def start
+        puts "Hello!"
+    end
+
+end
